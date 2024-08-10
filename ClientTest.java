e66cbb01846457e0c3f6584d0332dd0347766954
lenny <lennytmp@gmail.com>
2024-08-10 07:41:40 +0200

    Add unit tests for Client

---
+++ ClientTest.java
@@ -0,0 +1,31 @@
+package FightLang;
+
+public class ClientTest {
+    public static void main(String[] args) {
+        testNextExpLevel();
+    }
+
+    // A simple assertEquals function to compare expected and actual values
+    public static boolean assertEquals(int expected, int actual, String testName) {
+        if (expected == actual) {
+            System.out.print("S");
+            return true;
+        }
+        System.out.println(" Test: " + testName + " FAILED: expected " + expected + " but got " + actual);
+        return false;
+    }
+
+    public static void testNextExpLevel() {
+        Client c =  new Client(0, "test");
+        c.level = 1;
+        boolean allTestsPassed = true;
+        allTestsPassed &= assertEquals(30, c.nextExp(), "Expected experience for level 1 should be 30.");
+        c.level = 2;
+        allTestsPassed &= assertEquals(230, c.nextExp(), "Expected experience for level 2 should be 230.");
+        c.level = 3;
+        allTestsPassed &= assertEquals(530, c.nextExp(), "Expected experience for level 2 should be 530.");
+        if (!allTestsPassed) {
+            System.exit(1); 
+        }
+    }
+}
